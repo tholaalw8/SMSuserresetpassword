9acfc4d69e9b7a54a0ec4c9603a491331a4aa1bf
JS3015 <JS3015@198997D157>
2022-04-19 15:23:16 +0500

    Add project files.

---
+++ SMSuserresetpassword/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SMSuserresetpassword.Models;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Text;
+
+namespace SMSuserresetpassword.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+
+
+            User user = new User();
+
+
+           
+            user.getAduser();
+            Console.WriteLine("waiting....");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SMSuserresetpassword/Models/User.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SMSuserresetpassword.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public  async Task getAduser() {
+
+            const string fileName = "C:\\Users\\public\\data.txt";
+            
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            var start = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                Arguments = "get-aduser -filter * -property SamAccountName,Displayname,Officephone,mobilephone,office,distinguishedname,mail,lastlogondate,logonworkstations, lastlogontimestamp | where-object {$_.SamAccountName -eq 'js3015'} | select-object SamaccountName,Displayname,office, distinguishedname,mobilephone,officephone, mail  | convertto-json | out-file C:\\users\\public\\data.txt ",
+                CreateNoWindow = true
+            };
+
+            using var process = Process.Start(start);
+            using var reader = process.StandardOutput;
+
+            process.EnableRaisingEvents = true;
+           
+           /* var test = await reader.ReadToEndAsync();
+
+            foreach (String tx in test) {
+                test = test + "" + t;
+            }
+
+
+            await File.WriteAllLinesAsync(fileName,test);*/
+            await process.WaitForExitAsync();
+        }
+    
+
+    }
+}
